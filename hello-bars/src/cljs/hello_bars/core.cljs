c835f6e3ba7a37d0668dbb7d75cae6cb08099504
Tom White <tom@sixdozen.com>
2012-09-17 03:05:09 -0700

    hello-bars: A barebones c2 example
    A mashup of the existing todoMVC example and the current
    first example on the http://keminglabs.com/c2/ page.
    
    For the record, tweaks to the original example:
    
     * bind! the result to my html element
     * removed call to (style) [was throwing exception for me]
     * label's {:style "color: white;"} wasn't working, changed

---
+++ hello-bars/src/cljs/hello_bars/core.cljs
@@ -0,0 +1,17 @@
+(ns hello-bars.core
+  (:use-macros [c2.util :only [p pp bind!]])
+  (:use [c2.core :only [unify style]])
+  (:require [c2.scale :as scale]))
+
+(bind! "#bars" 
+  (let [width 500 bar-height 20
+        data {"A" 1, "B" 2, "C" 4, "D" 3}
+        s (scale/linear :domain [0 (apply max (vals data))]
+                        :range [0 width])]
+
+    [:div
+     (unify data (fn [[label val]]
+                   [:div {:style {:height bar-height
+                                  :width (s val)
+                                  :background-color "gray"}}
+                      [:span {:style {:color "white"}} label]]))]))
