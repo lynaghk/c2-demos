6b3b60d9392401a013e962e1dd743abf511400c1
tom <tom@wemomedia.com>
2012-10-02 18:59:26 -0700

    clocky: unify replaced with for, clock-atom now map
    Since order of processing is important, replaced unify
    with a for comprehension over the 4 keys. This means
    we can also drop the array-map and use a simple map.
    
    Note however that this version no longer works because
    the bind! macro no longer seems to be watching the
    clock-atom when it is not part of a unify. I'll
    followup with a separate commit that fixes this.

--- clocky/src/cljs/clocky/core.cljs
+++ clocky/src/cljs/clocky/core.cljs
@@ -8,36 +8,36 @@
 
 (def clock-atom
   "clock data bound to atom. each number is a percentage."
-  (atom (array-map :hours 50, :minutes 60, :seconds 70, :millis 80)))
+  (atom {:hours 50, :minutes 60, :seconds 70, :millis 80}))
 
 (def radii {:hours 275, :minutes 200, :seconds 110, :millis, 30})
 
 (bind! "#clocky" 
   [:svg
-   [:g {:transform "translate(300,300)rotate(-90)"}
-    (unify @clock-atom
-           (fn [[label val]]
-              (let [angle1 (/ (* Tau val) 100)
-                    angle2 (+ angle1 Pi)
-                    angle3 (+ angle2 Pi)
-                    radius (radii label)]
-                [:g.slice
-                  [:path {:class (str (name label) "1")
-                          :d (arc :outer-radius radius
-                                  :start-angle angle1
-                                  :end-angle   angle2)}]
-                  [:path {:class (str (name label) "2")
-                          :d (arc :outer-radius radius
-                                  :start-angle angle2
-                                  :end-angle   angle3)}]])))]])
+    [:g {:transform "translate(300,300)rotate(-90)"}
+      (for [k [:hours :minutes :seconds :millis]]
+        (let [v      (get @clock-atom k)
+              angle1 (/ (* Tau v) 100)
+              angle2 (+ angle1 Pi)
+              angle3 (+ angle2 Pi)
+              radius (get radii k)]
+          [:g.slice
+            [:path {:class (str (name k) "1")
+                    :d (arc :outer-radius radius
+                            :start-angle angle1
+                            :end-angle   angle2)}]
+            [:path {:class (str (name k) "2")
+                    :d (arc :outer-radius radius
+                            :start-angle angle2
+                            :end-angle   angle3)}]]))]])
 
 (defn nextloop []
   (let [d (js/Date.)]
-    (reset! clock-atom
-      (array-map :hours   (/ (* (rem (.getHours d) 12) 100) 12)
-                 :minutes (/ (* (.getMinutes d) 100) 60)
-                 :seconds (/ (* (.getSeconds d) 100) 60)
-                 :millis  (/ (* (.getMilliseconds d) 100) 1000)))))
+    (reset! clock-atom 
+      {:hours   (/ (* (rem (.getHours d) 12) 100) 12)
+       :minutes (/ (* (.getMinutes d) 100) 60)
+       :seconds (/ (* (.getSeconds d) 100) 60)
+       :millis  (/ (* (.getMilliseconds d) 100) 1000)})))
 
 (defn animation-loop []
   (.requestAnimationFrame (dom/getWindow) animation-loop)
